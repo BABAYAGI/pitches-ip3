1b29aad9358dbb29f86321a82df1293b98d831e3
babayagi <darsan_hirani@hotmail.com>
2019-10-31 15:29:16 +0300

    changes sqlalchemy_uri

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://darsan:qwerty@localhost/minutepitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://darsan:qwerty@localhost/minutepitch'
     UPLOADED_PHOTOS_DEST ='app/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # email configurations
